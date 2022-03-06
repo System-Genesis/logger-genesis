90250e9f7610a887321f81ef8dad32f2b1bc01ed
Roei <roeioren10@gmail.com>
2022-03-06 13:26:33 +0200

    add: types and class export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import LoggerGenesis from './logger';
+import { scopeOption, levelOptions } from './types';
 
 const logger: LoggerGenesis = new LoggerGenesis();
 
 export default logger;
+
+export { LoggerGenesis };
+
+export { scopeOption, levelOptions };
